9ae2d27fd3601473bfefdca34e710b872188901d
Katrina Janeczko <tuk04893@temple.edu>
2023-03-27 22:10:40 -0400

    Create handleInputString & update NOTES

--- NOTES.md
+++ NOTES.md
@@ -1,2 +1,4 @@
 Please make notes here to clarify any decisions taken that you wish to communicate, and capture any URLs you used in solving the problems at hand.
-
+* I didn't want .DS_Store to be added to my repo so I just added it to the .gitignore file
+* I notice that `run.sh` is not mentioned in the the README instructions. I did try `./run.sh` to run the Flask app, but I encountered an issue ("ImportError: cannot import name 'escape' from 'jinja2'"), but since using a web application framework isn't mentioned in the exercise instructions, I chose not to prioritize solving this issue.
+* I added comments for clarity and put the contents of the `else` block (which currently handles displaying the input string and its length) in its own function in anticipation of it becoming long & messy
--- python/app.py
+++ python/app.py
@@ -1,22 +1,37 @@
 
 seen_strings = {}
 
+
 def stringinate():
     while True:
-        inputResult = input('Enter an input string (\'stats\' to see statistics, \'quit\' to exit): ')
+        # Take user input
+        inputResult = input(
+            'Enter an input string (\'stats\' to see statistics, \'quit\' to exit): ')
+
+        # Handle "quit" input
         if (inputResult.lower() == "quit"):
             print('Exiting...\n')
             break
+
+        # Handle "stats" input
         elif (inputResult.lower() == "stats"):
             print('\nStats:\n %s\n' % seen_strings)
+
+        # Handle an input string
         else:
-            print('\nInput = %s' % inputResult)
-            print('Length = %s \n' % len(inputResult))
+            handleInputString(inputResult)
 
-            if inputResult in seen_strings:
-                seen_strings[inputResult] += 1
-            else:
-                seen_strings[inputResult] = 1
+def handleInputString(inputResult):
+    print('\nInput = %s' % inputResult)
+    print('Length = %s \n' % len(inputResult))
 
+    if inputResult in seen_strings:
+        seen_strings[inputResult] += 1
+    else:
+        seen_strings[inputResult] = 1
+        
 if __name__ == '__main__':
     stringinate()
+
+
+
